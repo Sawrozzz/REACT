ff7f954980377cd9450f808889402eccf953eaa3
[saroj adhikari] <sawroz.adhikari@gmail.com>
2024-01-06 20:59:38 +0545

    working on the projects

--- Portfolio/portfolio/src/Home.js
+++ Portfolio/portfolio/src/Home.js
@@ -2,22 +2,21 @@ import React from "react";
 const Home = () => {
   return (
     <>
-        <div class="home">
-          <div class="home-content">
-            <h1>Hi. I'm Saroj Adhikari</h1>
-            <h3>Frontend Developer</h3>
-            <p>
-              Yooo! Saroj Adhikari here, currently rocking BSc CSIT. I'm a tech
-              aficionado, a learning enthusiast, originally from Ilam but
-              soaking up the city life in Kathmandu. Join me as I navigate the
-              world of technology and embrace every learning opportunity that
-              comes my way!
-            </p>
-          </div>
-          <div className="image">
-            <img src="luffy.png" alt="image"></img>
-          </div>
+      <div class="home">
+        <div class="home-content">
+          <h1>Hi. I'm Saroj Adhikari</h1>
+          <h3>Frontend Developer</h3>
+          <p>
+            Yooo! Saroj Adhikari here, currently rocking BSc CSIT. I'm a tech
+            aficionado, a learning enthusiast, originally from Ilam but soaking
+            up the city life in Kathmandu. Join me as I navigate the world of
+            technology and embrace every learning opportunity that comes my way!
+          </p>
         </div>
+        <div className="image">
+        <img src="luffy.png" alt="image"></img>
+      </div>
+      </div>
     </>
   );
 };
